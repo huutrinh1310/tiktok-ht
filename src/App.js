1055f8c55050f9ade8f546ceb734f6e48f997a30
HuuTrinh <huutrinh1310@gmail.com>
2022-05-08 13:27:55 +0700

    Header UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '~/routes/route';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Upload() {
+    return (
+        <div>
+            <h1>Upload page</h1>
+        </div>
+    );
+}
+
+export default Upload;
